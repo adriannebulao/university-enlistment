facb063d752925add22e32c5c9f54042d1ec368d
Martina <martina1234angeles@gmail.com>
2024-06-06 16:09:03 +0800

    feat/create room tests

--- src/test/java/com/adriannebulao/enlistment/RoomTest.java
+++ src/test/java/com/adriannebulao/enlistment/RoomTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RoomTest {
     @Test
     void room_has_alphanumeric_name() {
-        Room room = new Room("A101", 20);
-        assertEquals("A101", room.getRoomName());
+        Room room = new Room("A102", 20);
+        assertEquals("A102", room.getRoomName());
         assertEquals(20, room.getRoomCapacity());
     }
 
